1964711bfd47d728595661eb8d9d1c42c5e8f3b4
a.savanovich <asavanster@gmail.com>
2026-08-05 11:15:31 +0300

    fix sha hash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.asavan.collect4"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
