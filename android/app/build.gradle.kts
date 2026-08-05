05b26502f7d00d25f9e33eb49f4e063e2d487f5b
a.savanovich <asavanster@gmail.com>
2026-08-05 23:56:50 +0300

    not use gradle deamon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.asavan.collect4"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
